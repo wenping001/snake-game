eca2f7e22f476dcd5dd36c7614b3f1c0509a52c1
wping <wen240979@gmail.com>
2022-04-17 13:31:04 +0800

    generate random dot.

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-const COLS = 15;
-const ROWS = 15;
+const COLS = 25;
+const ROWS = 18;
+const PIXEL = 30;
 
-const board = document.getElementById('board');
+const MIN = 0;
+const COL_MAX = 25 * 30;
+const ROW_MAX = 18 * 30;
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-for (let i = 0; i < COLS; i++) {
-  for (let j = 0; j < ROWS; j++) {
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    board.appendChild(cell);
+function init() {
+  for (let i = 0; i < ROWS; i++) {
+    for (let j = 0; j < COLS; j++) {
+      ctx.strokeStyle = 'blue';
+      ctx.strokeRect(j * PIXEL, i * PIXEL, PIXEL, PIXEL);
+    }
   }
 }
+
+function createRandomDot() {
+  const randomX = (Math.random() * 24).toFixed(0);
+  const randomY = (Math.random() * 17).toFixed(0);
+  console.log(randomX, randomY);
+  ctx.fillStyle = 'red';
+  ctx.fillRect(randomX * PIXEL, randomY * PIXEL, PIXEL, PIXEL);
+}
+
+function controllSnake() {}
+
+init();
+
+createRandomDot();
